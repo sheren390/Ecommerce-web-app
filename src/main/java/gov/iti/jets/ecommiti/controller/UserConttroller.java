97d337099dca3ed8522a286c494e8790d72a7761
Haidi99 <haidi.hany48@gmail.com>
2023-03-08 11:28:15 +0200

    auth

--- src/main/java/gov/iti/jets/ecommiti/controller/UserConttroller.java
+++ src/main/java/gov/iti/jets/ecommiti/controller/UserConttroller.java
@@ -2,19 +2,24 @@ package gov.iti.jets.ecommiti.controller;
 
 import gov.iti.jets.ecommiti.models.User;
 import gov.iti.jets.ecommiti.repositories.UserRepository;
+import gov.iti.jets.ecommiti.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/test")
 public class UserConttroller {
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @GetMapping("/users")
+//    @PreAuthorize("hasRole('ADMIN')")
     public List<User> getAll(){
-        return userRepository.findAll();
+        return userService.getAll();
     }
 }
